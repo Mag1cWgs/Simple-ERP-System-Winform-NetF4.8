5addd90e75c54345b8ab82dd9f514455f58ec312
Mag1cWgs <Mag1cWgs@outlook.com>
2025-02-22 02:18:58 +0800

    初始化项目，添加基础分层，添加基础窗体
    Signed-off-by: Mag1cWgs <Mag1cWgs@outlook.com>

---
+++ SimpleERPSystem/FrmHomePage.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace SimpleERPSystem
+{
+    partial class FrmHomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.SuspendLayout();
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(130, 153);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(90, 25);
+            this.kryptonButton1.TabIndex = 0;
+            this.kryptonButton1.Values.Text = "kryptonButton1";
+            // 
+            // FrmHomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kryptonButton1);
+            this.Name = "FrmHomePage";
+            this.Text = "FrmHomePage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonButton kryptonButton1;
+    }
+}
---
+++ SimpleERPSystem/FrmMain.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SimpleERPSystem
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dockPanelMain = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.vS2015LightTheme1 = new WeifenLuo.WinFormsUI.Docking.VS2015LightTheme();
+            this.SuspendLayout();
+            // 
+            // dockPanelMain
+            // 
+            this.dockPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dockPanelMain.DockBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(242)))));
+            this.dockPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.dockPanelMain.Name = "dockPanelMain";
+            this.dockPanelMain.Padding = new System.Windows.Forms.Padding(6);
+            this.dockPanelMain.ShowAutoHideContentOnHover = false;
+            this.dockPanelMain.Size = new System.Drawing.Size(800, 450);
+            this.dockPanelMain.TabIndex = 1;
+            this.dockPanelMain.Theme = this.vS2015LightTheme1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dockPanelMain);
+            this.IsMdiContainer = true;
+            this.Name = "FrmMain";
+            this.Text = "FrmMain";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WeifenLuo.WinFormsUI.Docking.DockPanel dockPanelMain;
+        private WeifenLuo.WinFormsUI.Docking.VS2015LightTheme vS2015LightTheme1;
+    }
+}
